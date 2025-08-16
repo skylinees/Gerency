d37032f64af979ac0483a8aed8fbd23693baae36
carloskauan <code.line.dark@gmail.com>
2025-08-15 21:29:35 -0300

    Inicialização de projeto

---
+++ main.js
@@ -0,0 +1,23 @@
+const { app, BrowserWindow, screen } = require('electron')
+
+function createMaximizedWindow() {
+  // Obtém as dimensões do monitor primário
+  const { width, height } = screen.getPrimaryDisplay().workAreaSize
+
+  const win = new BrowserWindow({
+    x: 0,  // Posição X no canto esquerdo
+    y: 0,  // Posição Y no topo
+    width,  // Largura igual à área útil do monitor
+    height, // Altura igual à área útil do monitor
+    maximizable: true,  // Permite maximizar
+    fullscreenable: false,  // Desativa o fullscreen tradicional
+    autoHideMenuBar: true,  // Opcional: esconde a barra de menu
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  win.loadFile('./src/views/index.html')
+}
+
+app.whenReady().then(createMaximizedWindow)
