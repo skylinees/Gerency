58d9961258a5beb1e3eb22d10113f84701eba335
carloskauan <code.line.dark@gmail.com>
2025-08-17 18:22:58 -0300

    Add menu personalizado e correções de segurança

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-const { app, BrowserWindow, screen } = require('electron')
+const { app, BrowserWindow, screen, Menu, shell } = require('electron')
 
-function createMaximizedWindow() {
+function createMainWindow() {
   // Obtém as dimensões do monitor primário
   const { width, height } = screen.getPrimaryDisplay().workAreaSize
 
@@ -9,15 +9,75 @@ function createMaximizedWindow() {
     y: 0,  // Posição Y no topo
     width,  // Largura igual à área útil do monitor
     height, // Altura igual à área útil do monitor
+    icon: './src/public/img/icon-paymate.png', // Adicionar icone,
     maximizable: true,  // Permite maximizar
     fullscreenable: false,  // Desativa o fullscreen tradicional
     autoHideMenuBar: true,  // Opcional: esconde a barra de menu
-    webPreferences: {
-      nodeIntegration: true
-    }
-  })
 
+  })
+  Menu.setApplicationMenu(Menu.buildFromTemplate(template))
   win.loadFile('./src/views/index.html')
 }
 
-app.whenReady().then(createMaximizedWindow)
+app.whenReady().then(createMainWindow)
+app.on("window-all-closed", () => {
+  if (process.platform !== 'darwin') app.quit()
+})
+
+const template = [
+  {
+    label: "Arquivo",
+    submenu: [{
+      label: 'Sair',
+      click: () => app.quit(),
+      accelerator: 'Alt+F4'
+    }]  
+  },
+  {
+    label: "Exibir",
+    submenu: [
+      {
+        label: "Recarregar",
+        role: "reload"
+      },
+      {
+        label: "Ferramentas do desenvolvedor",
+        role: "toggleDevTools"
+      },{type: "separator"},
+      {
+        label: "Zoom +",
+        role: "zoomIn"
+      },
+      {
+        label: "Zoom -",
+        role: "zoomOut"
+      },
+      {
+        label: "Restaurar zoom",
+        role: "resetZoom"
+      }
+    ]
+  },
+  {
+    label: "Ajuda",
+    submenu: [
+      {
+        label: "docs",
+        click: () => shell.openExternal("https://www.electronjs.org/pt/docs/latest/")
+      },{type: "separator"},
+      {
+        label: "Desenvolvedores",
+        submenu: [
+          {
+            label: "Front-End",
+            click: () => shell.openExternal("https://www.linkedin.com/in/wagnermarinho/")
+          },
+          {
+            label: "Back-End",
+            click: () => shell.openExternal("https://www.linkedin.com/in/carlos-kauan-brito-monteiro/")
+          },
+        ] 
+      }
+    ]
+  }
+]
